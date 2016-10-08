068f2e3b94e5c2389fb3826245cfac4f34ada35a
David <David@Meow>
2016-10-07 20:41:11 -0400

    new commit

---
+++ src/tablewrite/Course.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablewrite;
+
+/**
+ *
+ * @author David
+ */
+public class Course {
+    
+    private String courseCode;
+    private String courseName;
+    private String room;
+    private String teacher;
+    
+    Course(String courseCode, String courseName, String room, String teacher){
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.room = room;
+        this.teacher = teacher;
+    }
+    
+    String getCourseCode(){
+        return courseCode;
+    }
+    
+    String getCourseName(){
+        return courseName;
+    }
+    
+    String getRoom(){
+        return courseCode;
+    }
+    
+    String getTeacher(){
+        return courseCode;
+    }
+}
